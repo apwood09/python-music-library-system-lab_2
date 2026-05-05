0f7041def86ee852a232cd4313dd736d7ca50ba9
abbi portwood <apwood09@gmail.com>
2026-05-05 12:06:35 -0400

    Implement Song class with tracking attributes and class methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,54 @@
 class Song:
-    pass
+    # attributes: class
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # attrinutes: instance
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # trigger classmethods 
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
+
+    # CLASSMETHODS
+    # add song to count 
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    # add to generes
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    # add to artists
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    # add to genre count 
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    # add to artists count
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
